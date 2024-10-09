1f9e1a7a1383baa733be525b60cb75ae65fe286a
dhruvil <shahdhruvil420@gmail.com>
2024-10-09 16:49:22 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1234234;
+let accountEmail = "hitesh@gmail.com";
+var accountPassword ="12345";
+accountCity = "jaipur"; 
+
+ console.log(accountEmail = "kjhsdkj");
+ console.log(accountPassword ="u79847");
+ console.log(accountCity= "kjhijsdfkj")
+
+console.log ("accountId")
+
